424edc84128f48ed776d5339178ba7450a3616d2
paulckim <pkim7662@gmail.com>
2019-04-08 19:11:43 -0700

    save

--- java-snippets/src/main/java/com/snippets/algorithms/sorts/ImmutableListSort.java
+++
@@ -1,9 +0,0 @@
-package com.snippets.algorithms.sorts;
-
-import java.util.List;
-
-public interface ImmutableListSort<T extends Comparable<? super T>> {
-
-    List<T> sort(List<T> unsorted);
-
-}
--- java-snippets/src/main/java/com/snippets/algorithms/sorts/MutableListSort.java
+++
@@ -1,9 +0,0 @@
-package com.snippets.algorithms.sorts;
-
-import java.util.List;
-
-public interface MutableListSort<T extends Comparable<? super T>> {
-
-    void sort(List<T> unsorted);
-
-}
