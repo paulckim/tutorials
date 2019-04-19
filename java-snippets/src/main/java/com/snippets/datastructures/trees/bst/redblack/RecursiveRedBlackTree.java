cf002d4c6324a6010adb54a238daf762af941dc7
paulckim <pkim7662@gmail.com>
2019-04-19 01:38:28 -0700

    refactor recursive insterion

--- java-snippets/src/main/java/com/snippets/datastructures/trees/bst/redblack/RecursiveRedBlackTree.java
+++ java-snippets/src/main/java/com/snippets/datastructures/trees/bst/redblack/RecursiveRedBlackTree.java
@@ -4,31 +4,28 @@ import com.snippets.datastructures.trees.bst.AbstractTree;
 
 public class RecursiveRedBlackTree<T extends Comparable<? super T>> extends AbstractTree<T> {
 
+    private enum SubTreeState {
+        BALANCED, RECOLOR,
+        LL, LR, RR, RL
+    }
+
     @Override
     public void add(T value) {
         // EDGE-CASE 1:
         RBTreeNode<T> grandparent = (RBTreeNode) this.root;
         if(null == grandparent) {
-            this.root = new RBTreeNode(value, Color.BLACK);
+            this.root = new RBTreeNode<>(value, Color.BLACK);
             ++size;
             return;
         }
         // EDGE-CASE 2:
-        if(areValueEqual(value, grandparent.getValue())) return;
-        // EDGE-CASE 3:
-        boolean isLeft = isValueLess(value, grandparent.getValue());
-        RBTreeNode<T> parent = isLeft ? grandparent.getLeft() : grandparent.getRight();
-        if(null == parent) {
-            if(isLeft) grandparent.setLeft(new RBTreeNode<>(value));
-            else grandparent.setRight(new RBTreeNode<>(value));
-            ++size;
+        if(1 == size && addChild(grandparent, value))
             return;
-        }
-        // BASE-CASE:
+        // To continue, grandparent and parent cannot be equal to the value
+        if(isValueEqual(grandparent, value)) return;
+        // Begin Recursive Insertion:
         this.root = balancedAdd(grandparent, value);
-        Color rootColor = ((RBTreeNode<T>) this.root).getColor();
-        if(rootColor.equals(Color.RED))
-            ((RBTreeNode<T>) this.root).setColor(Color.BLACK);
+        colorRoot();
     }
 
     @Override
@@ -37,111 +34,154 @@ public class RecursiveRedBlackTree<T extends Comparable<? super T>> extends Abst
     }
 
     private RBTreeNode<T> balancedAdd(RBTreeNode<T> grandparent, T value) {
-        // Get the parent node of current subtree scope
-        RBTreeNode<T> parent = isValueLess(value, grandparent.getValue())
+        // If the child matches the value, the node already exists in the tree:
+        RBTreeNode<T> parent = isValueLess(grandparent, value)
                 ? grandparent.getLeft() : grandparent.getRight();
-        // If an existing descendant node matches <value>:
-        if(areValueEqual(value, parent.getValue())) return grandparent;
-        // Check if child node exists:
-        RBTreeNode<T> child = isNodeLeft(parent, grandparent)
+        if(isValueEqual(parent, value)) return grandparent;
+        RBTreeNode<T> child = isValueLess(parent, value)
                 ? parent.getLeft() : parent.getRight();
-        if(null == child)
-            return addNewNode(parent, grandparent, value);
-        // subtreeRoot is the new parent:
+        if(null == child) {
+            child = new RBTreeNode<>(value);
+            if(isNodeLeft(parent, grandparent)) parent.setLeft(child);
+            else parent.setRight(child);
+            ++size;
+            return balance(child, parent, grandparent);
+        }
         RBTreeNode<T> subtreeRoot = balancedAdd(parent, value);
         if(isNodeLeft(subtreeRoot, grandparent))
             grandparent.setLeft(subtreeRoot);
         else
             grandparent.setRight(subtreeRoot);
-
-        RBTreeNode<T> subtreeChild;
-        if(null != subtreeRoot.getLeft() && subtreeRoot.getLeft().getColor().equals(Color.RED))
-            subtreeChild = subtreeRoot.getLeft();
-        else if(null != subtreeRoot.getRight() && subtreeRoot.getRight().getColor().equals(Color.RED))
-            subtreeChild = subtreeRoot.getRight();
-        else
-            return grandparent;
-        RBTreeNode<T> balancedSubtree = balance(subtreeChild, subtreeRoot, grandparent);
-        if(null == balancedSubtree) return grandparent;
-        return balancedSubtree;
+        RBTreeNode<T> postChild = isValueLess(subtreeRoot, value)
+                ? subtreeRoot.getLeft() : subtreeRoot.getRight();
+        return balance(postChild, subtreeRoot, grandparent);
     }
 
-    private RBTreeNode<T> addNewNode(RBTreeNode<T> parent, RBTreeNode<T> grandparent, T value) {
-        RBTreeNode<T> child = new RBTreeNode<>(value);
-        if(isNodeLeft(parent, grandparent)) parent.setLeft(child);
-        else parent.setRight(child);
-        recolor(grandparent);
-        ++size;
-        RBTreeNode<T> subtreeRoot = balance(child, parent, grandparent);
-        if(null == subtreeRoot) return grandparent;
-        return subtreeRoot;
+    private RBTreeNode<T> balance(RBTreeNode<T> child, RBTreeNode<T> parent, RBTreeNode<T> grandparent) {
+        switch (getBalanceState(child, parent, grandparent)) {
+            case LL:
+                return rotateLeftLeft(parent, grandparent);
+            case LR:
+                return rotateLeftRight(child, parent, grandparent);
+            case RL:
+                return rotateRightLeft(child, parent, grandparent);
+            case RR:
+                return rotateRightRight(parent, grandparent);
+            case RECOLOR:
+                recolor(grandparent);
+            default:
+                return grandparent;
+        }
     }
 
-    private void recolor(RBTreeNode<T> parent) {
-        RBTreeNode<T> left = parent.getLeft();
-        RBTreeNode<T> right = parent.getRight();
-        if(null == left || null == right) return;
-        boolean needsColoring = (left.getColor().equals(Color.RED))
-                && (right.getColor().equals(Color.RED));
-        if(!needsColoring) return;
-        parent.setColor(Color.RED);
+    private SubTreeState getBalanceState(RBTreeNode<T> child, RBTreeNode<T> parent, RBTreeNode<T> grandparent) {
+        if(isNodeBlack(child) || isNodeBlack(parent)) return SubTreeState.BALANCED;
+        if(!isNodeBlack(child)
+                && !isNodeBlack(grandparent.getLeft())
+                && !isNodeBlack(grandparent.getRight()))
+            return SubTreeState.RECOLOR;
+        if(isNodeLeft(parent, grandparent)) {
+            if(isNodeLeft(child, parent)) return SubTreeState.LL;
+            else return SubTreeState.LR;
+        }
+        else {
+            if(isNodeLeft(child, parent)) return SubTreeState.RL;
+            else return SubTreeState.RR;
+        }
+    }
+
+    /********************
+     * Recoloring Rule:
+     ********************/
+
+    private void recolor(RBTreeNode<T> grandparent) {
+        grandparent.setColor(Color.RED);
+        RBTreeNode<T> left = grandparent.getLeft();
+        RBTreeNode<T> right = grandparent.getRight();
         left.setColor(Color.BLACK);
         right.setColor(Color.BLACK);
     }
 
-    private RBTreeNode<T> balance(RBTreeNode<T> child,
-                                  RBTreeNode<T> parent,
-                                  RBTreeNode<T> grandparent) {
-        if(child.getColor().equals(Color.BLACK)
-                || parent.getColor().equals(Color.BLACK))
-            return null;
-        RBTreeNode<T> subtreeRoot;
-        if(isNodeLeft(parent, grandparent)) {
-            if(isNodeLeft(child, parent)) {
-                subtreeRoot = parent;
-                grandparent.setLeft(parent.getRight());
-                parent.setRight(grandparent);
-            }
-            else {
-                subtreeRoot = child;
-                parent.setRight(child.getLeft());
-                grandparent.setLeft(child.getRight());
-                child.setLeft(parent);
-                child.setRight(grandparent);
-            }
-        }
-        else {
-            if(isNodeLeft(child, parent)) {
-                subtreeRoot = child;
-                grandparent.setRight(child.getLeft());
-                parent.setLeft(child.getRight());
-                child.setLeft(grandparent);
-                child.setRight(parent);
-            }
-            else {
-                subtreeRoot = parent;
-                grandparent.setRight(parent.getLeft());
-                parent.setLeft(grandparent);
-            }
-        }
-        subtreeRoot.setColor(Color.BLACK);
-        if(null != subtreeRoot.getLeft())
-            subtreeRoot.getLeft().setColor(Color.RED);
-        if(null != subtreeRoot.getRight())
-            subtreeRoot.getRight().setColor(Color.RED);
-        return subtreeRoot;
+    /********************
+     * Rotation Rule:
+     ********************/
+
+    private RBTreeNode<T> rotateLeftLeft(RBTreeNode<T> parent, RBTreeNode<T> grandparent) {
+        grandparent.setLeft(parent.getRight());
+        parent.setRight(grandparent);
+        colorRotation(parent);
+        return parent;
     }
 
-    private boolean areValueEqual(T left, T right) {
-        return left.compareTo(right) == 0;
+    private RBTreeNode<T> rotateLeftRight(RBTreeNode<T> child, RBTreeNode<T> parent, RBTreeNode<T> grandparent) {
+        parent.setRight(child.getLeft());
+        grandparent.setLeft(parent.getRight());
+        child.setLeft(parent);
+        child.setRight(grandparent);
+        colorRotation(child);
+        return child;
     }
 
-    private boolean isValueLess(T left, T right) {
-        return left.compareTo(right) < 0;
+    private RBTreeNode<T> rotateRightLeft(RBTreeNode<T> child, RBTreeNode<T> parent, RBTreeNode<T> grandparent) {
+        parent.setLeft(child.getRight());
+        grandparent.setRight(child.getLeft());
+        child.setLeft(grandparent);
+        child.setRight(parent);
+        colorRotation(child);
+        return child;
+    }
+
+    private RBTreeNode<T> rotateRightRight(RBTreeNode<T> parent, RBTreeNode<T> grandparent) {
+        grandparent.setRight(parent.getLeft());
+        parent.setLeft(grandparent);
+        colorRotation(parent);
+        return parent;
+    }
+
+    private boolean addChild(RBTreeNode<T> parent, T value) {
+        if(null == parent || isValueEqual(parent, value))
+            return false;
+        if(isValueLess(parent, value))
+            parent.setLeft(new RBTreeNode<>(value));
+        else
+            parent.setRight(new RBTreeNode<>(value));
+        ++size;
+        return true;
+    }
+
+    /**
+     * Takes into account null nodes (considered black).
+     *
+     * @param node
+     * @return
+     */
+    private boolean isNodeBlack(RBTreeNode<T> node) {
+        return null == node || node.getColor().equals(Color.BLACK);
     }
 
     private boolean isNodeLeft(RBTreeNode<T> child, RBTreeNode<T> parent) {
         return child.getValue().compareTo(parent.getValue()) < 0;
+    }
+
+    private boolean isValueLess(RBTreeNode<T> root, T value) {
+        return value.compareTo(root.getValue()) < 0;
+    }
+
+    private boolean isValueEqual(RBTreeNode<T> root, T value) {
+        return value.compareTo(root.getValue()) == 0;
+    }
+
+    private void colorRotation(RBTreeNode<T> root) {
+        root.setColor(Color.BLACK);
+        root.getLeft().setColor(Color.RED);
+        root.getRight().setColor(Color.RED);
+    }
+
+    private void colorRoot() {
+        // always keep root's color black:
+        Color rootColor = ((RBTreeNode<T>) this.root).getColor();
+        if(rootColor.equals(Color.RED))
+            ((RBTreeNode<T>) this.root).setColor(Color.BLACK);
     }
 
 }
